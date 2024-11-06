b7ab7afcffff5379d4ab392ec9d7f41bc3d7dd2f
N1kita14 <nikita141299.n@yandex.ru>
2024-11-06 20:01:03 +0300

    add +Makefile

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,14 @@ package hexlet.code;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        if(args[0].equals("-h")){
+            System.out.println("Usage: gendiff [-hV] [-f=format] filepath1 filepath2\n" +
+                    "Compares two configuration files and shows a difference.\n" +
+                    "      filepath1         path to first file\n" +
+                    "      filepath2         path to second file\n" +
+                    "  -f, --format=format   output format [default: stylish]\n" +
+                    "  -h, --help            Show this help message and exit.\n" +
+                    "  -V, --version         Print version information and exit.");
+        }
     }
 }
