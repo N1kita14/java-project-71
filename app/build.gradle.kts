2b45e973ddcf1d4d9b6d5da69d703101fec2d2a4
N1kita14 <nikita141299.n@yandex.ru>
2024-11-13 18:52:17 +0300

    add new files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation ("info.picocli:picocli:4.7.6")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,17 +1,49 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+@Command(name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class App {
+public class App{
+    @Parameters(paramLabel = "file1", description = "path to first file")
+    private String filePath1;
+
+    @Parameters(paramLabel = "file2", description = "path to second file")
+    private String filePath2;
+
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish", paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
     public static void main(String[] args) {
-        if(args[0].equals("-h")){
-            System.out.println("Usage: gendiff [-hV] [-f=format] filepath1 filepath2\n" +
-                    "Compares two configuration files and shows a difference.\n" +
-                    "      filepath1         path to first file\n" +
-                    "      filepath2         path to second file\n" +
-                    "  -f, --format=format   output format [default: stylish]\n" +
-                    "  -h, --help            Show this help message and exit.\n" +
-                    "  -V, --version         Print version information and exit.");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+    public static String json1Load(String file1){
+        try (BufferedReader str = new BufferedReader(new FileReader(file1))){
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ((line = str.readLine()) != null) {
+                builder.append(line);
+            }
+            System.out.println(builder.toString());
+            return builder.toString();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
