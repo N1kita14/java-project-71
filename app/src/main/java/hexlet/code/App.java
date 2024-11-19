7c81a12496844fa6bd1dba0f96e3ef96e1151bdf
N1kita14 <nikita141299.n@yandex.ru>
2024-11-19 20:11:11 +0300

    add new App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -9,6 +11,8 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Map;
+
 
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
@@ -16,7 +20,7 @@ import java.io.IOException;
         description = "Compares two configuration files and shows a difference.")
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.*/
 public class App{
     @Parameters(paramLabel = "file1", description = "path to first file")
     private String filePath1;
@@ -31,6 +35,15 @@ public class App{
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
+    /*public static void main(String[] args) throws JsonProcessingException {
+        String jsonLoadRun = json1Load("C:\\file1.json");
+        //System.out.println(jsonLoadRun);
+        Map<String, Object> mapJson = parsJson(jsonLoadRun);
+        String jsonLoadRun2 = json1Load("C:\\file2.json");
+        //System.out.println(jsonLoadRun2);
+        Map<String, Object> mapJson2 = parsJson(jsonLoadRun2);
+        System.out.println(Differ.generate(mapJson, mapJson2));
+    }*/
     public static String json1Load(String file1){
         try (BufferedReader str = new BufferedReader(new FileReader(file1))){
             String line;
@@ -38,12 +51,16 @@ public class App{
             while ((line = str.readLine()) != null) {
                 builder.append(line);
             }
-            System.out.println(builder.toString());
             return builder.toString();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static Map<String, Object> parsJson(String json) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String,Object> map = objectMapper.readValue(json, Map.class);
+        return map;
     }
 }
