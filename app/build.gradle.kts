49546c1a77bc4749c7869086d698f02cb11de74d
N1kita14 <nikita141299.n@yandex.ru>
2024-11-19 21:10:55 +0300

    add new files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     implementation ("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
     annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,4 +1,33 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Differ {
+    public static String generate(Map<String, Object> diffFile1, Map<String, Object> diffFile2) throws JsonProcessingException {
+        Map<String, Object> file12 = new TreeMap<>();
+        for(String key: diffFile2.keySet()){
+            if(diffFile1.containsKey(key)){
+                if(diffFile1.get(key).equals(diffFile2.get(key))){
+                    file12.put(key, diffFile1.get(key));
+                }else{
+                    file12.put("- " + key, diffFile1.get(key));
+                    file12.put("+ " + key, diffFile2.get(key));
+                }
+            }else{
+                file12.put("+ " + key, diffFile2.get(key));
+            }
+        }
+        for(String key: diffFile1.keySet()){
+            if(!diffFile2.containsKey(key)){
+                file12.put("- " + key, diffFile1.get(key));
+            }
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(file12);
+    }
 }
