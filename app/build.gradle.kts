5898e8eaed6db8b166f7ba5ff9c703cf02fe1701
N1kita14 <nikita141299.n@yandex.ru>
2024-12-12 21:28:43 +0300

    updated files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,43 +1,28 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     application
+    checkstyle
     jacoco
-    id("checkstyle")
-    //id("io.freefair.lombok") version "8.6"
-    id("com.github.ben-manes.versions") version "0.50.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.freefair.lombok") version "8.4"
 }
-
-group = "io.hexlet"
-
-version = "1.0-SNAPSHOT"
-
 application { mainClass.set("hexlet.code.App") }
 
-repositories { mavenCentral() }
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-collections4:4.4")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.0-rc1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc1")
     implementation("info.picocli:picocli:4.7.6")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.1")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
-    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
-    testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        // showStackTraces = true
-        // showCauses = true
-        showStandardStreams = true
-    }
-}
 
+}
 tasks.jacocoTestReport { reports { xml.required.set(true) } }
