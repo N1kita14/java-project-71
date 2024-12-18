551e7ef66971efef0bcb93d5d29005eed4e46a09
N1kita14 <nikita141299.n@yandex.ru>
2024-12-18 21:36:16 +0300

    Правки

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,11 +14,12 @@ repositories {
 }
 
 dependencies {
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.0-rc1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc1")
-    implementation("info.picocli:picocli:4.7.6")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation ("com.fasterxml.jackson.core:jackson-databind:2.15.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
 }
 
 tasks.test {
