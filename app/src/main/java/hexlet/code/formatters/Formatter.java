358d59faef381f5453134d97bdfb577eda329328
N1kita14 <nikita141299.n@yandex.ru>
2024-12-19 21:38:00 +0300

    update Json and Formatter

--- app/src/main/java/hexlet/code/formatters/Formatter.java
+++ app/src/main/java/hexlet/code/formatters/Formatter.java
@@ -1,19 +1,15 @@
 package hexlet.code.formatters;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 public class Formatter {
     public static String format(Map<String, Object> diffFile1, Map<String, Object> diffFile2, String styles) throws JsonProcessingException {
-    return switch(styles){
-        case "stylish" -> Stylish.format(diffFile1, diffFile2);
-        case "json" -> Json.format(diffFile1, diffFile2);
-        case "plain" -> Plain.format(diffFile1, diffFile2);
-        default -> throw new RuntimeException("unknown differ format");
-    };
+        return switch(styles){
+            case "stylish" -> Stylish.format(diffFile1, diffFile2);
+            case "json" -> Json.format(diffFile1, diffFile2);
+            case "plain" -> Plain.format(diffFile1, diffFile2);
+            default -> throw new RuntimeException("unknown differ format");
+        };
     }
 }
-
--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -7,10 +7,10 @@ import java.util.*;
 
 public class Json {
     public static String format(Map<String, Object> diffFile1, Map<String, Object> diffFile2) throws JsonProcessingException {
-        List<Map<String, Object>> list = new ArrayList<>();
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
         list.add(diffFile1);
         list.add(diffFile2);
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(list);
+        return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(list);
     }
 }
