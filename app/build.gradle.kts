8cea2fe8ad07ac716fb6484894133b3e35690858
N1kita14 <nikita141299.n@yandex.ru>
2024-12-22 18:14:49 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("info.picocli:picocli:4.7.6")
-    implementation("com.fasterxml.jackson.core:jackson-databind:1.9.9")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:1.9.9")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.17.2")
 }
 
 tasks.test {
