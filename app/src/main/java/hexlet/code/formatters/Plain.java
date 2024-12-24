cea7d5a66fcfea82a21a3619361a93a410a70161
N1kita14 <nikita141299.n@yandex.ru>
2024-12-24 19:09:43 +0300

    update Plain

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -5,28 +5,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.ArrayList;
 import java.util.Map;
-import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Plain {
     public static String format(Map<String, Object> diffFile1, Map<String, Object> diffFile2) throws Exception {
-        ArrayList<String> file12 = new ArrayList<>();
-        for(String key: diffFile2.keySet()){
-            if(diffFile1.containsKey(key)){
+
+        StringBuilder difference = new StringBuilder();
+
+        TreeSet<String> keys = new TreeSet<>();
+        keys.addAll(diffFile1.keySet());
+        keys.addAll(diffFile2.keySet());
+
+        for(String key: keys){
+            if(diffFile1.containsKey(key) && diffFile2.containsKey(key)){
                 if(diffFile1.get(key) != null && diffFile2.get(key) != null && diffFile1.get(key).equals(diffFile2.get(key))){
-                    file12.add("Property " + key + " was updated" + ". " + "From " + diffFile1.get(key) + " to " + diffFile2.get(key));
+                    difference.append("Property ").append(key).append(" was update").append(". ").append("From ").append(diffFile1.get(key)).append(" to ").append(diffFile2.get(key)).append(System.lineSeparator());
                 }else{
-                    file12.add("Property " + key + " was added with value: [" + diffFile2.get(key) + "].");
+                    difference.append("Property ").append(key).append(" was added with value: [").append(diffFile2.get(key)).append("].").append(System.lineSeparator());
                 }
             }else{
-                file12.add("Property " + key + " was updated" + ". " + "From " + diffFile1.get(key) + " to "  + diffFile2.get(key));
+                difference.append("Property ").append(key).append(" was update").append(". ").append("From ").append(diffFile1.get(key)).append(" to ").append(diffFile2.get(key)).append(System.lineSeparator());
             }
         }
         for(String key: diffFile1.keySet()){
             if(!diffFile2.containsKey(key)){
-                file12.add("Property " + key + " was removed.");
+                difference.append("Property ").append(key).append(" was removed.").append(System.lineSeparator());
             }
         }
-        ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(file12).replace(",", "\n");
+        return difference.toString();
     }
 }
