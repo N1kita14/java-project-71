d9490078717de528d333f0e348990cd566a4a54c
N1kita14 <nikita141299.n@yandex.ru>
2024-12-24 19:14:26 +0300

    update Plain

--- app/src/main/java/hexlet/code/formatters/Plain.java
+++ app/src/main/java/hexlet/code/formatters/Plain.java
@@ -19,17 +19,17 @@ public class Plain {
         for(String key: keys){
             if(diffFile1.containsKey(key) && diffFile2.containsKey(key)){
                 if(diffFile1.get(key) != null && diffFile2.get(key) != null && diffFile1.get(key).equals(diffFile2.get(key))){
-                    difference.append("Property ").append(key).append(" was update").append(". ").append("From ").append(diffFile1.get(key)).append(" to ").append(diffFile2.get(key)).append(System.lineSeparator());
+                    difference.append("Property ").append("'").append(key).append("'").append(" was update").append(". ").append("From ").append(diffFile1.get(key)).append(" to ").append(diffFile2.get(key)).append(System.lineSeparator());
                 }else{
-                    difference.append("Property ").append(key).append(" was added with value: [").append(diffFile2.get(key)).append("].").append(System.lineSeparator());
+                    difference.append("Property ").append("'").append(key).append("'").append(" was added with value: [").append(diffFile2.get(key)).append("].").append(System.lineSeparator());
                 }
             }else{
-                difference.append("Property ").append(key).append(" was update").append(". ").append("From ").append(diffFile1.get(key)).append(" to ").append(diffFile2.get(key)).append(System.lineSeparator());
+                difference.append("Property ").append("'").append(key).append("'").append(" was update").append(". ").append("From ").append(diffFile1.get(key)).append(" to ").append(diffFile2.get(key)).append(System.lineSeparator());
             }
         }
         for(String key: diffFile1.keySet()){
             if(!diffFile2.containsKey(key)){
-                difference.append("Property ").append(key).append(" was removed.").append(System.lineSeparator());
+                difference.append("Property ").append("'").append(key).append("'").append(" was removed.").append(System.lineSeparator());
             }
         }
         return difference.toString();
