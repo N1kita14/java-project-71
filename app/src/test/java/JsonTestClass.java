c2b1a66cb7bbe3e2f07420d3ccc065ba895d0b53
N1kita14 <nikita141299.n@yandex.ru>
2025-01-07 20:34:27 +0300

    update JsonTestClass add filepath11.yml add filepath22.yml add PlainExpected.txt add StylishExpected.txt add JsonExpected.txt

--- app/src/test/java/JsonTestClass.java
+++ app/src/test/java/JsonTestClass.java
@@ -1,62 +1,52 @@
+import hexlet.code.Differ;
 
-import hexlet.code.Pars;
-import hexlet.code.formatters.Stylish;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonTestClass {
+    private static String resultJson;
+    private static String resultPlain;
+    private static String resultStylish;
 
-    @Test
-    void testDiffJson() {
-        String testFile1 = "{\n"
-                + "  \"host\": \"hexlet.io\",\n"
-                + "  \"timeout\": 50,\n"
-                + "  \"proxy\": \"123.234.53.22\",\n"
-                + "  \"follow\": false\n"
-                + "}";
-        String testFile2 = "{\n"
-                + "  \"timeout\": 20,\n"
-                + "  \"verbose\": true,\n"
-                + "  \"host\": \"hexlet.io\"\n"
-                + "}";
-        try {
-            Map<String, Object> testJson1 = Pars.pars(testFile1, "json");
-            Map<String, Object> testJson2 = Pars.pars(testFile2, "json");
-            String result = "{\"+ timeout\":20,"
-                    + "\"+ verbose\":true,"
-                    + "\"- follow\":false,"
-                    + "\"- proxy\":\"123.234.53.22\","
-                    + "\"- timeout\":50,"
-                    + "\"host\":\"hexlet.io\"}";
-            Assertions.assertEquals(result, Stylish.format(testJson1, testJson2));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @BeforeAll
+    public static void beforeAll() throws Exception {
+        resultJson = readFixture("JsonExpected.txt");
+        resultPlain = readFixture("PlainExpected.txt");
+        resultStylish = readFixture("StylishExpected.txt");
     }
 
-    @Test
-    void testFileYaml() {
-        String testFile1Yaml = "host: hexlet.io\n"
-                + "timeout: 50\n"
-                + "proxy: 123.234.53.22\n"
-                + "follow: false";
-        String testFile2Yaml = "timeout: 20\n"
-                + "verbose: true\n"
-                + "host: hexlet.io\n";
-        try {
-            Map<String, Object> testYaml1 = Pars.pars(testFile1Yaml, "yaml");
-            Map<String, Object> testYaml2 = Pars.pars(testFile2Yaml, "yaml");
-            String resultYaml = "{\"+ timeout\":20,"
-                    + "\"+ verbose\":true,"
-                    + "\"- follow\":false,"
-                    + "\"- proxy\":\"123.234.53.22\","
-                    + "\"- timeout\":50,"
-                    + "\"host\":\"hexlet.io\"}";
-            Assertions.assertEquals(resultYaml, Stylish.format(testYaml1, testYaml2));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+    @ParameterizedTest
+    @ValueSource(strings = {"json", "yml"})
+    public void generateTest(String format) throws Exception {
+        String filePath1 = getFixturePath("file1." + format).toString();
+        String filePath2 = getFixturePath("file2." + format).toString();
+
+        assertEquals(resultStylish ,Differ.generate(filePath1, filePath2));
+
+        assertEquals(resultStylish ,Differ.generate(filePath1, filePath2, "stylish"));
+
+        assertEquals(resultPlain ,Differ.generate(filePath1, filePath2, "plain"));
+
+        assertEquals(resultJson ,Differ.generate(filePath1, filePath2, "json"));
+
     }
+
+
+    private static Path getFixturePath(String fileName) {
+        return Paths.get("src", "test", "resources", "fixtures", fileName)
+                .toAbsolutePath().normalize();
+    }
+
+    private static String readFixture(String fileName) throws Exception {
+        Path filePath = getFixturePath(fileName);
+        return Files.readString(filePath).trim();
+    }
+
 }
